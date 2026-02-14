f019a9348a697ebaaa1e04f3eb692ff1ff77df1c
aarju <aarjubasnet270@gmail.com>
2026-02-15 04:14:40 +0545

    fixing errors

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true only for port 465
+  port: 465,
+  secure: true, // true only for port 465
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
